22d3107eb8e0512b6dc85bf8df0df5b31efad76b
thuyngocha <hatn@dgroup.co>
2022-07-18 16:09:41 +0700

    add: tooltip delete button

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,9 +1,11 @@
 import icon_bananasign_logo from './icon_bananasign_logo.svg';
 import icon_cancel_button from './icon_cancel-button.svg';
 import icon_person from './icon_person.svg';
+import icon_repeat from './icon_repeat.svg';
 import icon_show from './icon_show.svg';
 import icon_sign_logo from './icon_sign_logo.svg';
 import icon_sign from './icon_sign.svg';
+import icon_trash from './icon_trash.svg';
 
 export {
   icon_cancel_button,
@@ -12,4 +14,6 @@ export {
   icon_show,
   icon_bananasign_logo,
   icon_person,
+  icon_trash,
+  icon_repeat,
 };
--- src/components/InviteToSign/InviteUser/AssignerItem/AssignerItem.tsx
+++ src/components/InviteToSign/InviteUser/AssignerItem/AssignerItem.tsx
@@ -1,7 +1,9 @@
 import './AssignerItem.style.scss';
 
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
+import {Images} from '@src/assets';
 import Avatar from '@src/components/Avatar';
 
 const AssignerItem: React.FC = () => {
@@ -11,14 +13,22 @@ const AssignerItem: React.FC = () => {
   return (
     <div className="AssignerItem__container">
       <div className="AssignerItem__wrapper">
-        {/* avatar */}
         <Avatar user={user} className="AssignerItem__wrapper-avatar" />
         <div className="AssignerItem__wrapper-name">
           <p>{'Guest User'}</p>
           <p>{'user.email'}</p>
         </div>
       </div>
-      <div className="AssignerItem__delete-button">aaa</div>
+      <div data-tip="Remove signer" className="AssignerItem__delete-button">
+        <img src={Images.icon_trash} alt="delete icon" />
+        <ReactTooltip
+          offset={{top: 4, right: 40}}
+          effect="solid"
+          arrowColor="transparent"
+          place="bottom"
+          className="AssignerItem__tooltip"
+        />
+      </div>
     </div>
   );
 };
