d0a4356b79e6719bba95dae342e2f0bdcb254c98
Thanh Tung <tungp@dgroup.co>
2022-07-21 15:21:17 +0700

    fix: close iframe

--- dist/components/InviteToSign/BananasignIframe/BananasignIframe.js
+++ dist/components/InviteToSign/BananasignIframe/BananasignIframe.js
@@ -1,54 +1,87 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
+var __assign =
+  (this && this.__assign) ||
+  function () {
+    __assign =
+      Object.assign ||
+      function (t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
+          s = arguments[i];
+          for (var p in s)
+            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
         }
         return t;
-    };
+      };
     return __assign.apply(this, arguments);
-};
-import { jsx as _jsx } from "react/jsx-runtime";
+  };
+
 import './BananasignIframe.style.scss';
-import { useContext, useEffect } from 'react';
+
+import {useContext, useEffect} from 'react';
+import {jsx as _jsx} from 'react/jsx-runtime';
+
 import InviteToSignContext from '../InviteToSignContext';
-import { InviteToSignContextActions } from '../InviteToSignContextActions';
+import {InviteToSignContextActions} from '../InviteToSignContextActions';
 var CLOSE_TASK_BANANA_SIGN_MSG = 'close_task';
 var BACK_STEP_BANANA_SIGN_MSG = 'back_step';
 var BananasignIframe = function () {
-    var context = useContext(InviteToSignContext);
-    var _a = context.state, openBananasignIframe = _a.openBananasignIframe, onClose = _a.onClose, identify = _a.identify, bananasignUrl = _a.bananasignUrl, dispatch = context.dispatch;
-    var listenToBananaSign = function (e) {
-        if (e.origin === bananasignUrl) {
-            switch (e.data.type) {
-                case CLOSE_TASK_BANANA_SIGN_MSG:
-                    onClose();
-                    break;
-                case BACK_STEP_BANANA_SIGN_MSG:
-                    dispatch(InviteToSignContextActions.OPEN_BANANASIGN_IFRAME(false));
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
-    useEffect(function () {
-        window.addEventListener('message', listenToBananaSign, false);
-        return function () {
-            window.removeEventListener('message', listenToBananaSign);
-        };
-    }, []);
-    if (!openBananasignIframe || !identify) {
-        return null;
+  var context = useContext(InviteToSignContext);
+  var _a = context.state,
+    openBananasignIframe = _a.openBananasignIframe,
+    onClose = _a.onClose,
+    identify = _a.identify,
+    bananasignUrl = _a.bananasignUrl,
+    dispatch = context.dispatch;
+  var listenToBananaSign = function (e) {
+    if (e.origin === bananasignUrl) {
+      switch (e.data.type) {
+        case CLOSE_TASK_BANANA_SIGN_MSG:
+          dispatch(InviteToSignContextActions.OPEN_BANANASIGN_IFRAME(false));
+          onClose();
+          break;
+        case BACK_STEP_BANANA_SIGN_MSG:
+          dispatch(InviteToSignContextActions.OPEN_BANANASIGN_IFRAME(false));
+          break;
+        default:
+          break;
+      }
     }
-    return (_jsx("div", __assign({ className: "BananasignIframe__container" }, { children: _jsx("iframe", { width: "100%", height: "100%", src: "".concat(bananasignUrl, "/embed/").concat(identify), title: "Iframe Upload", frameBorder: "0", allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture", id: "bananaSignIframe", onLoad: function () {
-                var elementIframe = document.getElementById('bananaSignIframe');
-                if (elementIframe) {
-                    elementIframe.style.visibility = 'visible';
-                }
-                dispatch(InviteToSignContextActions.SET_LOADING(false));
-            }, style: { visibility: 'hidden' }, allowFullScreen: true }) })));
+  };
+  useEffect(function () {
+    window.addEventListener('message', listenToBananaSign, false);
+    return function () {
+      window.removeEventListener('message', listenToBananaSign);
+    };
+  }, []);
+  if (!openBananasignIframe || !identify) {
+    return null;
+  }
+  return _jsx(
+    'div',
+    __assign(
+      {className: 'BananasignIframe__container'},
+      {
+        children: _jsx('iframe', {
+          width: '100%',
+          height: '100%',
+          src: ''.concat(bananasignUrl, '/embed/').concat(identify),
+          title: 'Iframe Upload',
+          frameBorder: '0',
+          allow:
+            'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
+          id: 'bananaSignIframe',
+          onLoad: function () {
+            var elementIframe = document.getElementById('bananaSignIframe');
+            if (elementIframe) {
+              elementIframe.style.visibility = 'visible';
+            }
+            dispatch(InviteToSignContextActions.SET_LOADING(false));
+          },
+          style: {visibility: 'hidden'},
+          allowFullScreen: true,
+        }),
+      },
+    ),
+  );
 };
 export default BananasignIframe;
 //# sourceMappingURL=BananasignIframe.js.map
